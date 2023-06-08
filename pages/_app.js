787a95df8da8266bd17918e0ddf9ce81771d9fef
yoni goldshtein <yoni7022@gmail.com>
2023-06-08 11:16:41 +0300

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+     <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KSJ9FSNXWZ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KSJ9FSNXWZ');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
